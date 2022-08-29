4e601b2d52d1c8882ffe120e550f9a8c3dc57b80
MarcoBeraldo <beraldomar@gmail.com>
2022-08-29 17:20:40 +0200

    completed

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 $paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit. ";
 $paragraph_length = strlen($paragraph);
 $word = $_GET['word'];
-$censored = str_replace( $word, "***", $paragraph );
-$censored_length = strlen($censored)
+$censored_paragraph = str_replace( $word, "***", $paragraph );
+$censored_length = strlen($censored_paragraph)
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,7 +17,7 @@ $censored_length = strlen($censored)
 <p><strong>Paragrafo: </strong><?= $paragraph ?></p>
 <p><strong>Lunghezza:</strong> <?= $paragraph_length ?> caratteri.</p>
 <p><strong>Parola da censurare:</strong>  <?= $word ?>.</p>
-<p><strong>Testo censurato:</strong><?= $censored ?></p>
+<p><strong>Testo censurato:</strong><?= $censored_paragraph ?></p>
 <p><strong>Lunghezza testo censurato:</strong><?= $censored_length ?></p>
 </body>
 </html>
